57c88289e5619a0e18e072f9f892d19863d770b5
Kalvin Zhao <kalvinzhao@kalvins-mbp.home>
2020-08-18 13:04:03 -0400

    added function to update and submit form

--- src/components/App.js
+++ src/components/App.js
@@ -35,18 +35,37 @@ export default function App() {
   const [friends, setFriends] = useState([]) // careful what you initialize your state to
 
   // 🔥 STEP 1 - WE NEED STATE TO HOLD ALL VALUES OF THE FORM!
-  const formValues = {} // fix this using the state hook
+  // const formValues = {} // fix this using the state hook
+  const {formValues, setFormsValue} = useState(initialFormValues)
 
   const updateForm = (inputName, inputValue) => {
     // 🔥 STEP 2 - IMPLEMENT a "form state updater" which will be used inside the inputs' `onChange` handler
     //  It takes in the name of an input and its value, and updates `formValues`
+    setFormsVal({...initialFormValues, [inputName] : inputValue})
   }
 
   const submitForm = () => {
     // 🔥 STEP 3 - IMPLEMENT a submit function which will be used inside the form's own `onSubmit`
     //  a) make a new friend object, trimming whitespace from username and email
+    const friend = { 
+      // id: uuid() // its the backend that take cares of unique ID
+      username: formValues.username().trim,
+      email: formValues.email().trim(),
+      role: formValues.role,
+    }
     //  b) prevent further action if either username or email or role is empty string after trimming
+    if (!friend.username || !friend.email) return 
     //  c) POST new friend to backend, and on success update the list of friends in state with the new friend from API
+    fakeAxiosPost('fake.com', friend)
+      .then(res =>{
+        setFriends([res.data, ...friends])
+      })
+      .catch(error =>{
+        debugger
+      })
+      .finally(()=>{
+        setFormsValue(initialFormValues)
+      })
     //  d) also on success clear the form
   }
 
@@ -62,7 +81,7 @@ export default function App() {
         // 🔥 STEP 4 - The form component needs its props.
         //  Check implementation of FriendForm
         //  to see what props it expects.
-        values={{}}
+        values={formValues} update={updateForm} submit={submitForm}
       />
 
       {
